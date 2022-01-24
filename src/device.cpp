a051db2efa9342af78356f195ec78b3ad79e8f33
Wxz234 <429373960@qq.com>
2022-01-24 19:39:20 +0800

    finish CheckDXError

--- src/device.cpp
+++ src/device.cpp
@@ -1,7 +1,7 @@
 #include "device.h"
-
+#include "dx12_backend.h"
 namespace SmartFCCS {
 	Device::Device() {
-		D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&m_Device));
+		CheckDXError(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&m_Device)));
 	}
 }
--- src/dx12_backend.cpp
+++ src/dx12_backend.cpp
@@ -1,1 +1,8 @@
 #include "dx12_backend.h"
+namespace SmartFCCS {
+	void CheckDXError(HRESULT hr) {
+		if (hr != S_OK) {
+			throw "DX ERROR!";
+		}
+	}
+}
