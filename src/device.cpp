4341b26ad2ef9311ebff4558264d369abdabdbe7
Wxz <429373960@qq.com>
2022-02-04 00:54:28 +0800

    add EnableDebugLayer

--- src/device.cpp
+++ src/device.cpp
@@ -5,6 +5,14 @@
 #include "dx12_backend.h"
 namespace SmartFCCS {
 	Device::Device() {
+
+#if defined(_DEBUG)
+		Microsoft::WRL::ComPtr<ID3D12Debug> debugController;
+		if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
+		{
+			debugController->EnableDebugLayer();
+		}
+#endif
 		CheckDXError(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(&m_Device)));
 	}
 
