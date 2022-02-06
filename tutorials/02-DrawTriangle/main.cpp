43c10ad88bfbe1e809bc863809e33517153b9e8a
Wxz <429373960@qq.com>
2022-02-07 04:00:33 +0800

    Update main.cpp

--- tutorials/02-DrawTriangle/main.cpp
+++ tutorials/02-DrawTriangle/main.cpp
@@ -2,6 +2,7 @@
 #include <SmartFCCS/SmartFCCS.h>
 #include <d3dcompiler.h>
 #include <wrl/client.h>
+#include <dxgi1_2.h>
 using namespace SmartFCCS;
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
@@ -26,7 +27,41 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     Microsoft::WRL::ComPtr<ID3D11InputLayout> layout;
     device_ptr->CreateInputLayout(desc, 1, vs_blob->GetBufferPointer(), vs_blob->GetBufferSize(), &layout);
 
+    float triangleVertices[3][4] = {
+        { 0.00f, 0.25f, 0.00f, 1.00f },
+        { 0.25f,-0.25f, 0.00f, 1.00f },
+        {-0.25f,-0.25f, 0.00f, 1.00f },
+    };
+    D3D11_BUFFER_DESC vbd = {};
+    vbd.Usage = D3D11_USAGE_IMMUTABLE;
+    vbd.ByteWidth = sizeof(triangleVertices);
+    vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    vbd.CPUAccessFlags = 0;
+    D3D11_SUBRESOURCE_DATA InitData = {};
+    InitData.pSysMem = triangleVertices;
+    Microsoft::WRL::ComPtr<ID3D11Buffer> vertexbuffer;
+    device_ptr->CreateBuffer(&vbd, &InitData, &vertexbuffer);
+
+    uint32_t stride = sizeof(triangleVertices) / 3;
+    uint32_t offset = 0;
+    auto immediateContext = device->GetDeviceContext();
+    immediateContext->IASetVertexBuffers(0, 1, vertexbuffer.GetAddressOf(), &stride, &offset);
+    immediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    immediateContext->IASetInputLayout(layout.Get());
+    immediateContext->VSSetShader(vs.Get(), nullptr, 0);
+    immediateContext->PSSetShader(ps.Get(), nullptr, 0);
+
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> backBuffer;
+    Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView;
+    auto swapchain_ptr = reinterpret_cast<IDXGISwapChain*>(swapchain->GetNativePointer());
+    swapchain_ptr->GetBuffer(0, IID_PPV_ARGS(&backBuffer));
+    device_ptr->CreateRenderTargetView(backBuffer.Get(), nullptr, &renderTargetView);
+
     while (window->IsRun()) {
+        float black[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+        immediateContext->ClearRenderTargetView(renderTargetView.Get(), black);
+
+       // immediateContext->DrawInstanced()
         swapchain->Present();
     }
     return 0;
