49b4b87ab8a5a47d339ad465a542802bcd0278c6
Wxz <429373960@qq.com>
2022-02-11 01:12:04 +0800

    update IDeviceContext

--- include/SmartFCCS/Camera.h
+++ include/SmartFCCS/Camera.h
@@ -2,12 +2,20 @@
 #include "Core.h"
 namespace SmartFCCS {
 	struct Camera {
-		Camera(): pos(0.f,0.f,0.f) {}
+		Camera() : pos(0, 0, 0), dir(0, 0, 1), up(0, 1, 0) {}
 		Camera(const Camera& camera) = default; 
 		Camera& operator=(const Camera& camera) = default;
 		Camera(Camera&& camera) noexcept = default;
 		Camera& operator=(Camera&& camera) noexcept = default;
-
+		void f() {
+			DirectX:: XMMatrixPerspectiveFovLH
+		}
 		DirectX::XMFLOAT3 pos;
+		DirectX::XMFLOAT3 dir;
+		DirectX::XMFLOAT3 up;
+		float FovAngleY;
+		float AspectRatio;
+		float NearZ;
+		float FarZ;
 	};
 }
