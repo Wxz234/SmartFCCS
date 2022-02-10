8b0a604f150284a6a6671c43b1c126a04dc85797
Wxz234 <429373960@qq.com>
2022-02-10 19:41:07 +0800

    Update Camera.h

--- include/SmartFCCS/Camera.h
+++ include/SmartFCCS/Camera.h
@@ -1,5 +1,13 @@
 #pragma once
 #include "Core.h"
 namespace SmartFCCS {
+	struct Camera {
+		Camera() {}
+		Camera(const Camera& camera) = default; 
+		Camera& operator=(const Camera& camera) = default;
+		Camera(Camera&& camera) noexcept = default;
+		Camera& operator=(Camera&& camera) noexcept = default;
 
+		DirectX::XMFLOAT4 pos;
+	};
 }
