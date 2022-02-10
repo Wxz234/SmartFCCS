36216397db5324d58c07f0b90675e382c31213fe
Wxz234 <429373960@qq.com>
2022-02-10 19:45:17 +0800

    Update Camera.h

--- include/SmartFCCS/Camera.h
+++ include/SmartFCCS/Camera.h
@@ -2,7 +2,7 @@
 #include "Core.h"
 namespace SmartFCCS {
 	struct Camera {
-		Camera() {}
+		Camera(): pos(0.f,0.f,0.f) {}
 		Camera(const Camera& camera) = default; 
 		Camera& operator=(const Camera& camera) = default;
 		Camera(Camera&& camera) noexcept = default;
