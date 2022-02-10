ff7e89140ca981e987b625837f4d693afeec3a47
Wxz234 <429373960@qq.com>
2022-02-10 14:07:18 +0800

    update t2 t1

--- tutorials/01-InitializeDevice/main.cpp
+++ tutorials/01-InitializeDevice/main.cpp
@@ -4,9 +4,9 @@ using namespace SmartFCCS;
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
     auto window = CreateWindowF(L"fccs", 800, 600);
+    window->ShowWindow();
     auto device = CreateDevice();
     auto swapchain = CreateSwapChain(window.get(), device.get(), DXGI_FORMAT_R8G8B8A8_UNORM);
-    window->ShowWindow();
     while (window->IsRun()) {
         swapchain->Present();
     }
