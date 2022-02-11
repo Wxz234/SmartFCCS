a5843d0b868b53d344c86299218b20028c210131
Wxz234 <429373960@qq.com>
2022-02-11 14:43:46 +0800

    Update Camera.h

--- include/SmartFCCS/Camera.h
+++ include/SmartFCCS/Camera.h
@@ -2,14 +2,25 @@
 #include "Core.h"
 namespace SmartFCCS {
 	struct Camera {
-		Camera() : pos(0, 0, 0), dir(0, 0, 1), up(0, 1, 0) {}
+		Camera(const DirectX::XMFLOAT3 & pos, const DirectX::XMFLOAT3& dir, const DirectX::XMFLOAT3& up, float FovAngleY, float AspectRatio, float NearZ, float FarZ)
+		{
+			this->pos = pos;
+			this->dir = dir;
+			this->up = up;
+			this->FovAngleY = FovAngleY;
+			this->AspectRatio = AspectRatio;
+			this->NearZ = NearZ;
+			this->FarZ = FarZ;
+		}
 		Camera(const Camera& camera) = default; 
 		Camera& operator=(const Camera& camera) = default;
 		Camera(Camera&& camera) noexcept = default;
 		Camera& operator=(Camera&& camera) noexcept = default;
 		void f() {
-			DirectX:: XMMatrixPerspectiveFovLH
+			//DirectX:: XMMatrixPerspectiveFovLH
+				
 		}
+	private:
 		DirectX::XMFLOAT3 pos;
 		DirectX::XMFLOAT3 dir;
 		DirectX::XMFLOAT3 up;
