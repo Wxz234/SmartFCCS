502fa3aa9339685f6fac3d1863652e7bae649f86
Wxz <429373960@qq.com>
2022-02-15 01:29:47 +0800

    Update main.cpp

--- tutorials/04-MultithreadedRendering/main.cpp
+++ tutorials/04-MultithreadedRendering/main.cpp
@@ -8,11 +8,17 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
     auto device = CreateDevice();
     auto swapchain = CreateSwapChain(window.get(), device.get(), DXGI_FORMAT_R8G8B8A8_UNORM);
     //Create Deffered Contexts
-    auto immediatecontext = device->GetDefaultDeviceContext();
-    auto copycontext = device->CreateDeviceContext();
-    auto rendercontext = device->CreateDeviceContext();
+    auto immediateContext = device->GetDefaultDeviceContext();
+    auto copyContext = device->CreateDeviceContext();
+    auto renderContext = device->CreateDeviceContext();
+
+    auto immediateContext_ptr = reinterpret_cast<ID3D11DeviceContext*>(immediateContext->GetNativePointer());
+    auto copyContext_ptr = reinterpret_cast<ID3D11DeviceContext*>(copyContext->GetNativePointer());
+    auto renderContext_ptr = reinterpret_cast<ID3D11DeviceContext*>(renderContext->GetNativePointer());
     while (window->IsRun()) {
         window->DispatchWindowMessage();
+
+
         swapchain->Present();
     }
     return 0;
